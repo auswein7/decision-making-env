5ef0b06dfdacc6b8627d1501fc5f34fffc350290
Austin Weingart <austinweingart@Austins-MacBook-Pro.local>
2025-01-22 16:34:57 -0700

    small change

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,16 +1,19 @@
-# add more utility functions to this file in the future to represent more advanced agent action allocation
-
 #TODO:: Rework this file, either remove or clean up the interaction of this function with System
 def system_utility(agent, action, system):
     """
-    Update the agent's current action with the current candidate action.
+    Test the system score with a potential agent action
 
     :param agent: current 'awake' agent
     :param action: the current candidate action for the agent
     :param system: current problem layout
-    :return: the score of the system after taking this action
+    :return: the score of the system if this action is taken
     """
+    previous_action = agent.current_action
     agent.current_action = action
-    return system.system_score()
+
+    sys_score = system.system_score()
+
+    agent.current_score = previous_action
+    return sys_score
 
 
