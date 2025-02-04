bc975664de014b61ad3d9ad43899a2c0a1b61efe
Austin Weingart <austinweingart@Austins-MacBook-Pro.local>
2025-02-04 09:10:28 -0700

    remove unused func

--- app/core/data_collector.py
+++ app/core/data_collector.py
@@ -1,7 +1,5 @@
 import json
-import pprint
 from copy import deepcopy
-from typing import final
 
 from app.core.algorithms import brute_force
 from app.utils.common_utils import format_agent_data
@@ -9,21 +7,6 @@ from app.utils.common_utils import format_agent_data
 class DataCollector:
     def __init__(self):
         self.results = []
-
-    # metrics
-    # 1. Brute Force Score vs Attained score
-    # 2. Agent resource changes, action count
-    # 3. Percentage of resources covered
-    # 4. Average agents per resource
-    # 5. over coverage of agents (agents on r > M)
-    # do under coverage as well
-    # 6. resource volatility, how often was this resource covered or not
-    # 7. agent contribution (algorithmic fairness)
-    # 9. agent action utilization (how many actions are "unused", not contributing to overall score)
-    # 11. overhead actions, how many actions did not improve system score?
-    # 12. system convergence time, use metric 8
-    # 13. agent load balance, is one agent acting more often than others?
-    # 14. agent net contribution? +,- to score as sim is running
 
     def log(self, trial, iteration, system):
         self.results.append({
@@ -92,7 +75,7 @@ class DataCollector:
                 "over_covered_resources": over_coverage_map,
                 "max_possible_score": brute_force_score,
                 "simulation_score": sim_score,
-                "grade": str(sim_score/brute_force_score)+"%",
+                "grade": str((sim_score/brute_force_score)*100)+"%",
                 "agent_total_actions": DataCollector.count_agent_actions(data=data),
                 "resource_popularity":DataCollector.calculate_resource_popularity(data=data),
                 "agent_contributions": DataCollector.calculate_agent_contribution(agents=agents,
@@ -104,7 +87,7 @@ class DataCollector:
             })
 
             output_str = "sim_results_file_" + file_names[trial-1][-36:]
-            # DataCollector.save_to_json(sim_summary, output_str)
+
             with open(output_str, "w") as file:
                 json.dump(sim_summary, file, indent=4)
 
@@ -190,16 +173,3 @@ class DataCollector:
                 agent_actions[agent.id] = agent.current_action
 
         return agent_action_counts
-
-    @staticmethod
-    def save_to_json(data, filename="simulation_results.json"):
-        json_data = json.dumps(data)
-        formatted_json = pprint.pformat(json_data, sort_dicts=False)
-
-        with open(filename, "w") as f:
-            f.write(formatted_json)
-
-    @staticmethod
-    def format_json(json_string, indent=2):
-        data = json.loads(json_string)
-        return json.dumps(data, indent=indent)
